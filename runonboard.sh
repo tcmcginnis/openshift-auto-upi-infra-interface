02e607bb2c2b7d5cc38327c8e77cb0f08f3d8017
root <tmcginni@redhat.com>
2025-07-28 18:43:27 -0400

    Finish node config provision code

--- runonboard.sh
+++ runonboard.sh
@@ -1,2 +1,9 @@
 cd `dirname $0`
-ansible-playbook -i inventory/sample-ocp4.yml ./demo-onboard.yaml $*
+ansible-playbook ./demo-onboard.yaml \
+ -e cluster_name="ocp4-test" \
+ -e appcode="xyz" \
+ -e domain_name="mclabs.us" \
+ -e mastercount=3 \
+ -e infracount=3 \
+ -e workercount=3 \
+$*
