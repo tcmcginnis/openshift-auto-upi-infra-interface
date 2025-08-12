b0c1395b7359634060348449d5b7922367b40a9a
root <tmcginni@redhat.com>
2025-08-12 16:19:18 -0400

    Replace netmask with cidr

--- runonboard-demo.sh
+++ runonboard-demo.sh
@@ -3,6 +3,7 @@ ansible-playbook ./demo-onboard.yaml \
  -e cluster_name="ocp4-test" \
  -e appcode="xyz" \
  -e domain_name="mclabs.us" \
+ -e cluster_network_cidr="10.100.59.0/24" \
  -e mastercount=3 \
  -e infracount=3 \
  -e workercount=3 \
